4918a1932ccc2a283a899a7eafd5d1b7a5d2a3f9
Ally <69776903+ahamil53@users.noreply.github.com>
2020-11-19 22:26:07 -0500

    Update MealnBudgetPlanner.py
    starting the input to accept the meal request

--- MealnBudgetPlanner.py
+++ MealnBudgetPlanner.py
@@ -1,1 +1,12 @@
+import itertools
+import yaml
 
+docs = yaml.load(open("recipes.yml", "r"))
+options = dict((i, (name, values)) for i, (name, values) in enumerate(docs.items(), 1))
+
+print "What would you like to meal prep?"
+print "Here are the options:"
+for option, (name, ingredients) in iter(sorted(options.iteritems())):
+    print str(option) + ". " + str(name)
+
+choose = raw_input("> ")
