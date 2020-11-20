764ac0685cbe96769923bd1a2afb8c5252dfd48c
Ally <69776903+ahamil53@users.noreply.github.com>
2020-11-19 22:33:00 -0500

    Update MealnBudgetPlanner.py

--- MealnBudgetPlanner.py
+++ MealnBudgetPlanner.py
@@ -5,7 +5,7 @@ docs = yaml.load(open("recipes.yml", "r"))
 options = dict((i, (name, values)) for i, (name, values) in enumerate(docs.items(), 1))
 
 print "What would you like to meal prep?"
-print "Here are the options:"
+print "Here are the options based on the budget provided:"
 for option, (name, ingredients) in iter(sorted(options.iteritems())):
     print str(option) + ". " + str(name)
 
