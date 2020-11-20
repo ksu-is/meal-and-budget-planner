185d8161bcd3334037db9616531075c8a5a5fec9
kmusgro1 <69818638+kmusgro1@users.noreply.github.com>
2020-11-20 09:20:23 -0500

    Create recipe_picker.py
    The code we were basing our project on was not woking as we had hoped. We have developed this code as a simplified version. It does not take user input to create the recipie lists. The user can choose from recipies already set in the code. This code is original and was not taken from another  users repo
    
    Co-Authored-By: Ally <69776903+ahamil53@users.noreply.github.com>

---
+++ recipe_picker.py
@@ -0,0 +1,48 @@
+#recipe list  = name, cost per serving, and ingredients
+rec1 = ["baked potato",2.50,'potato','butter','cheese','bacon']
+rec2 = ["spaghetti",2.00,'spaghetti noodles','tomato sauce','hamburger']
+rec3 = ["grilled chicken",1.50,'chicken breast','marinade']
+
+#list of recipe
+recipes = [rec1,rec2,rec3]
+
+week = ["mon","tues","wed","thur","fri","sat","sun"]
+
+#user info
+user_budget = int(input('What is you budget for the week? '))
+user_servings = int(input('How many people are you feeding? '))
+
+#print all the recipe available
+print('choose from the available recipes')
+for list in recipes:
+    print(list[0])
+
+#set up peramiters
+meal = ""
+meal_cost = 0
+total_cost = 0
+grocery_list = [] #<----figure out how to remove duplicates and add multiples
+#for each day of the week select a meal from the printed recipe list
+#for each list in the recipes list look for the selected meal
+#when true choose the second list item ie the cost and multiply by the user_servings
+#add the meal cost to the total cost
+#add the ingredents to the grocery list
+for day in week:
+    meal = input("Meal - Select from list: ")
+    for list in recipes:
+        if list[0] == meal.lower():
+            #print(list[1])
+            meal_cost = list[1] * int(user_servings)
+            #print(meal_cost)
+            total_cost += meal_cost
+            #print(total_cost)
+            grocery_list.append(list[2::])
+            print("budget remaining", int(user_budget)-total_cost)
+        else:
+            pass
+    
+print("total cost for the week:",total_cost)
+print("grocery list",grocery_list)
+
+
+
