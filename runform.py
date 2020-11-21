4c67e9a70dac20c2d75bd4a39d193c6009ada5fc
kmusgro1 <69818638+kmusgro1@users.noreply.github.com>
2020-11-20 19:05:53 -0500

    Update runform.py

--- runform.py
+++ runform.py
@@ -2,11 +2,12 @@
 #testing to run the UI thrugh flask
 from flask import Flask, render_template, request
 app = Flask(__name__)
-
+### this is the form
 @app.route('/')
 def student():
    return render_template('student.html')
 
+### this is the table on the next page
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
