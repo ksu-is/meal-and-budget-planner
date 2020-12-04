5a5c72374313fcba1b3495ac77345a06d3aab37f
Ally <69776903+ahamil53@users.noreply.github.com>
2020-12-04 16:28:22 -0500

    Update and rename Recommended Budget Calculator.py to Budget Calculator.py
    added to the budget calculator and included meal choices

--- Budget Calculator.py
+++ Budget Calculator.py
@@ -1,3 +1,22 @@
+meal_choice = {}
+meal_history = {}
+stop = 'go'
+while stop != 'q'
+    item_name = iput('Meal choice: ')
+    quantity = input("Total purchased: ")
+    meal_choice['name'] = item_name
+    meal_choice['number'] = int(quantity)
+    meal_choice['price'] = float(cost)
+    meal_history.append(meal_choice.copy())
+    stop = input("Would you like to choose a different meal?\nType 'c' for continue or 'q' to quit :")
+final_total = 0
+for index, item in enumerate(meal_history):
+    item_total = item['number'] * item['price']
+    final_total += item_total
+    print(item['number'], item['name'], item['price'], item_total))
+    item_total = 0
+print('Grand total: $%.2f' % final_total)
+
 recommendation_budget = 0
 
 def program():
@@ -10,13 +29,9 @@ def program():
         print()
         print('YOUR RECOMMENDED BUDGET CATEGORY PERCENTAGES!')        
         print('Housing is 25%:\n $', format(round(your_budget *.25,2), '.2f'))
-        print('Insurance is 15%:\n $', format(round(your_budget *.15,2), '.2f'))
         print('Food is 10%:\n $', format(round(your_budget *.10,2), '.2f'))
         print('Transportation is 5%:\n $', format(round(your_budget *.05,2), '.2f'))
         print('Utilities is 5%:\n $', format(round(your_budget *.05,2), '.2f'))
-        print('Savings is 15%:\n $', format(round(your_budget *.15,2), '.2f'))
-        print('Entertainment is 10%:\n $', format(round(your_budget *.10,2), '.2f'))
-        print('Giving is 10%:\n $', format(round(your_budget *.10,2), '.2f'))
         print('Personal is 5%:\n $', format(round(your_budget *.05,2), '.2f'))
         print()
         print('Menu:')
