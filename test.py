80c99e24a6325528967636de39be146efbfe2aaf
kmusgro1 <69818638+kmusgro1@users.noreply.github.com>
2020-12-11 19:50:36 -0500

    Moved Files
    Cleaned up repo so that old and unused files are now in the old files folder

--- test.py
+++
@@ -1,6 +0,0 @@
-recipe = {} 
-recipe['chicken'] = [2.50, 'chicken', 'oil', 'herbs'] #A valid key-value pair 
-recipe['pasta'] = [1.5, 'pasta','sauce'] #A valid key-value pair 
-recipe[3] = [['three', ['THREE', 'tHrEe']]] #It is valid too 
-for k, v in recipe.items(): 
-    print(k, '=>', v) 
